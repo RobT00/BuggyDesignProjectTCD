7abf03021a77656a7f7ca3c53aa09544d9250e5f
Alexandra Silva <asilva@tcd.ie>
2017-01-25 17:44:19 +0000

    Week 2 CS lab

--- Station/Station/Program.cs
+++ Station/Station/Program.cs
@@ -4,12 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Station
+namespace assignment_1_2e10
 {
     class Program
     {
+        static string message = "";
+        static bool isRunning = true;
+
         static void Main(string[] args)
         {
+            while(isRunning)
+            {
+                Console.WriteLine("Please enter control message: 'print:X' or 'reversePrint:X' or 'end'");
+                message = Console.ReadLine();
+                
+                if(message.StartsWith("print:"))
+                {
+
+                    int number = Int32.Parse(message.Substring(6, 1));
+                    for(int i=0; i<number; i++)
+                    {
+                        Console.WriteLine("print message: " + message.Substring(7));
+                    }
+                    
+                   
+                }
+                else if (message.StartsWith("reversePrint:"))
+                {
+                    int number = Int32.Parse(message.Substring(13, 1));
+                    for(int i=0; i<number; i++)
+                    {
+                        char[] array = message.Substring(14).ToCharArray();
+                        string reverse = string.Empty;
+                        for (int j = array.Length - 1; j > -1; j--)
+                        {
+                            reverse += array[j];
+                        }
+                        Console.WriteLine("print message: " + reverse);
+                    }
+                }
+                else if (message.StartsWith("end"))
+                {
+                    Console.WriteLine("message: end");
+                    isRunning = false;
+                }
+                else
+                {
+                    Console.WriteLine("message: " + message + "not valid");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
