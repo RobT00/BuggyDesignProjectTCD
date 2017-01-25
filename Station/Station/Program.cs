cb3cba7927eca59e6e2533d587bf4a06edfa2c26
Eoin Kavanagh <kavanae4@tcd.ie>
2017-01-25 17:33:19 +0000

    Week 2 CS lab

--- Station/Station/Program.cs
+++ Station/Station/Program.cs
@@ -4,12 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Station
+namespace Week_2_Lab_Z8_2
 {
     class Program
     {
+        static string message = "";
+        
+        static bool isRunning = true;
         static void Main(string[] args)
         {
+           while (isRunning)
+            {
+                Console.WriteLine("Please enter your control message: 'print:X' or 'stop' or 'reversePrint:Xmessage'");
+
+                message = Console.ReadLine();
+                string input = message;
+
+                if (message.StartsWith("print:"))
+                {
+                    int number = Int32.Parse(message.Substring(6, 1));
+
+                    for(int i=0; i<number; i++)
+                    {
+                        Console.WriteLine("print message: " + message.Substring(6));
+
+                    }
+                }
+
+                else if (message.StartsWith("stop"))
+                    {
+                        Console.WriteLine("message: stop");
+                        isRunning = false;
+                    }
+                else if (message.StartsWith("reversePrint"))
+                {
+                    
+                    string message = new string(input.ToCharArray().Reverse().ToArray());
+                    Console.WriteLine("print message: " + message);
+                }
+                else
+                {
+                        Console.WriteLine("message: " + message + " not valid");
+                     }
+                }
+
+                Console.ReadLine();
+            }
         }
-    }
 }
+
