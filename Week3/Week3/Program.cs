4656fd277ca532ee9e6b9f562e592ded9b7fd8bd
Unknown <trewr@tcd.ie>
2017-01-30 22:40:26 +0000

    Work done is CS Lab, Week 3
    C# code for the week 3 lab, asynchronous event handling and logging of chat

---
+++ Week3/Week3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Week3
+{
+    class Program
+    {
+        static String message = "";
+        static List<String> Messages = new List<String>();
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            SerialPort port = new SerialPort();
+            port.PortName = "COM6";
+            port.BaudRate = 9600;
+            port.Open();
+            port.DataReceived += recievedData;
+
+            port.Write("+++");
+            Thread.Sleep(1100);
+            port.WriteLine("ATID 3333, CH C, CN");
+            //Thread.Sleep(1100);
+            //port.DiscardInBuffer();
+
+            //port.Write("---");
+            while (true) {
+                Console.Write("> ");
+                message = Console.ReadLine();
+                if (message == "print")
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        string output = Messages[i];
+                        Console.WriteLine(output);
+                    }
+                }
+                else
+                {
+                    port.WriteLine(message);
+                    Messages.Add(message);
+                    count++;
+                }
+            }
+        }
+
+        private static void recievedData(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort test = (SerialPort) sender;
+            String message2 = test.ReadLine();
+            Console.WriteLine("> " + message2);
+            Messages.Add(message2);
+            count++;
+        }
+    }
+}
