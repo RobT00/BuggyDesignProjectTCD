3496a57bac040392c87a015eb52306aa4fbbe568
Unknown <asilva@tcd.ie>
2017-02-08 14:11:39 +0000

    bcc motot control class

---
+++ Buggy/motorControls/motorControls.ino
@@ -0,0 +1,50 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+class motorControl {
+
+#define bcc_pin 1;
+
+
+
+ 
+motorControl() {
+pinMode(bcc_pin, OUTPUT);
+digitalWrite(bcc_pin, LOW);
+
+}
+
+void leftOveride () {
+  digitalWrite(bcc_pin, HIGH);
+  delay(6);
+  digitalWrite(bcc_pin, LOW);
+  }
+
+void rightOveride () {
+  digitalWrite(bcc_pin, HIGH);
+  delay(8);
+  digitalWrite(bcc_pin, LOW);
+  }
+
+void go () {
+  digitalWrite(bcc_pin, HIGH);
+  delay(4);
+  digitalWrite(bcc_pin, LOW);
+  }
+
+void stopBuggy () {
+  digitalWrite(bcc_pin, HIGH);
+  delay(2);
+  digitalWrite(bcc_pin, LOW);
+  }    
+
+
+};
