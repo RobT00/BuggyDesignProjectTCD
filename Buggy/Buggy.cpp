a940b89500a2bc624b8785cbe5b46a38bb5256cc
Unknown <kavanae4@tcd.ie>
2017-02-13 17:54:25 +0000

    Lab 5

--- Buggy/Buggy.cpp
+++ Buggy/Buggy.cpp
@@ -8,11 +8,11 @@ void Buggy::go() {
     lastGoTime = millis();
 }
 
-void Buggy::stop() {
-    if (!isGoing) {
+void Buggy::stop1() {
+    if (isGoing) {
         return;
     }
-    motor.stop();
+    motor.stop1();
     travelledTime += millis() - lastGoTime;
 }
 
@@ -29,3 +29,46 @@ void Buggy::flashLED() const {
   delay(300);
   digitalWrite(LED_BUILTIN, LOW);
 }
+
+void Buggy::gantry_ISR(){
+  if(!atGantry){
+    irInterrupt = true;
+  }
+}
+
+void Buggy::detectGantry(){
+  if(atGantry && getTravelledTime() > 500){
+    atGantry = false;
+  }
+  if (irInterrupt){
+    int gantry = readGantry();
+    if (gantry == -1) {
+      comms.writeXbee("GANTRY_INVALID");
+      irInterrupt = false;
+    } else {
+      comms.writeXbee("GANTRY" + String(gantry));
+      irInterrupt = false;
+      atGantry = true;
+      stop1();
+      delay(1000);
+      go();
+    }
+  }
+}
+
+int Buggy::readGantry() const {
+  while(digitalRead(IR_PIN) != LOW);
+  int pulse = pulseIn(IR_PIN, HIGH);
+
+  if(pulse >= 500 && pulse <= 1500){
+    return 1;
+  }else if (pulse >= 1500 && pulse <= 2500){
+    return 2;
+  }else if (pulse >= 2500 && pulse <= 3500){
+    return 3;
+  } else {
+    return -1;
+  }
+}
+
+
--- Buggy/Buggy.ino
+++ Buggy/Buggy.ino
@@ -11,11 +11,14 @@ void setup() {
   comm.addHandler("PING", [] { comm.writeXbee("PONG"); });
   comm.addHandler("PONG", [] { comm.writeXbee("PING"); });
   comm.addHandler("LED", [] { buggy->flashLED(); });
+  comm.addHandler("GO", [] { buggy->go(); });
+  comm.addHandler("STOP", [] { buggy->stop1(); });
+
+  attachInterrupt(digitalPinToInterrupt(Buggy::IR_PIN), IR_ISR, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  buggy->detectGantry();
 }
 
 void serialEvent(){
@@ -24,3 +27,8 @@ void serialEvent(){
     comm.processCommand(message);
   }
 }
+
+void IR_ISR(){
+  buggy->gantry_ISR();
+}
+
