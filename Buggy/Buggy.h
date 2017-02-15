a11d48110ea7ce440662abd5cd1566b8a6d191ce
Aron Hoffmann <hoffmaar@tcd.ie>
2017-02-15 00:26:53 +0000

    Fixed Gantry detection issue

--- Buggy/Buggy.h
+++ Buggy/Buggy.h
@@ -6,37 +6,38 @@
 #include <Arduino.h>
 
 class Buggy {
-    private:
-        const CommTrans *comms;
-        const MotorControls motor;
+  private:
+    const CommTrans *comms;
+    const MotorControls motor;
 
-        bool isGoing = false;
-        unsigned long travelledTime = 0l;
-        unsigned long lastGoTime = 0l;
+    bool isGoing = false;
+    unsigned long travelledTime = 0l;
+    unsigned long lastGoTime = 0l;
  
-        volatile bool irInterrupt = false;
-        volatile bool underGantry = false;
-        unsigned long atGantryAt = 0l;
+    volatile bool irInterrupt = false;
+    volatile bool underGantry = false;
+    unsigned long atGantryAt = 0l;
 
-        int readGantry() const;
-        unsigned long timeTravelledSinceGantry() const;
+    int readGantry() const;
+    unsigned long timeTravelledSinceGantry() const;
 
-    public:
-        static const short IR_PIN = 2;
-    
-        Buggy() = delete;
-        Buggy(CommTrans *c) : comms(c) {
-            pinMode(IR_PIN, INPUT);
-            pinMode(LED_BUILTIN, OUTPUT);
-        };
+  public:
+    static const short IR_PIN = 2;
+    static const short LED_PIN = 13;
+  
+    Buggy() = delete;
+    Buggy(CommTrans *c) : comms(c) {
+      pinMode(IR_PIN, INPUT);
+      pinMode(LED_PIN, OUTPUT);
+    };
 
-        void go();
-        void stop();
+    void go();
+    void stop();
 
-        void flashLED() const;
+    void flashLED() const;
 
-        void gantry_ISR();
-        void detectGantry();
-        
-        unsigned long getTravelledTime() const;
+    void gantry_ISR();
+    void detectGantry();
+
+    unsigned long getTravelledTime() const;
 };
